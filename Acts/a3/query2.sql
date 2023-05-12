91206cf1e0aaceee3ca42cd5fbf3a7d7e9573dcf
Alexis <40745653+Adoyle210@users.noreply.github.com>
2023-05-12 15:25:37 -0700

    Add files via upload

---
+++ Acts/a3/query2.sql
@@ -0,0 +1,15 @@
+-- For each category of film, find the name of the category (as "category_name") and the count of films (as "number_of_films") that MAE HOFFMAN has stared in within that category. 
+-- Order by the category name in descending order. 
+-- (Note: your query should return every category even if MAE has been in no films in that category--i.e., her count is 0 for "number_of_films" for that respective "category_name").
+
+-- Put query for Q2 here
+
+SELECT cate.name AS category_name, COUNT(act.actor_id) AS number_of_films FROM category cate
+    LEFT JOIN film_category AS fc ON cate.category_id=fc.category_id
+    LEFT JOIN film AS f ON fc.film_id=f.film_id
+    LEFT JOIN film_actor AS fa ON f.film_id=fa.film_id 
+    LEFT JOIN actor AS act ON 
+        (fa.actor_id=act.actor_id) AND 
+        (act.first_name='MAE' AND act.last_name='HOFFMAN')
+GROUP BY cate.category_id
+ORDER BY cate.name DESC;
