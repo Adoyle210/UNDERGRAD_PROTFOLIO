8c3e927f6c13c100131679f0d800692c81f97d2b
Adoyle210 <40745653+Adoyle210@users.noreply.github.com>
2023-05-25 21:13:50 -0700

    more stuff

--- Project/DDL.sql
+++ Project/DDL.sql
@@ -27,7 +27,7 @@ DROP TABLE IF EXISTS pokedex;
 CREATE TABLE pokedex (
     pname varchar(25) NOT NULL,
     ptype int NOT NULL,     
-    pokedexID int NOT NULL,
+    pokedexID int NOT NULL AUTO_INCREMENT,
     regionID int NOT NULL,
     PRIMARY KEY (pokedexID)
     
@@ -69,27 +69,27 @@ INSERT INTO region (kanto) VALUES ('true');
 INSERT INTO region (johto) VALUES ('true');
 INSERT INTO region (kanto, johto) VALUES ('true', 'true');
 
-INSERT INTO pokedex (pname, ptype, pokedexID, regionID) 
-    VALUES ('bulbasaur', 1, 1, (SELECT id FROM region WHERE kanto = 'true')),
-        ('ivysaur', 1, 2, (SELECT id FROM region WHERE kanto = 'true')),
-        ('venusaur', 1, 3, (SELECT id FROM region WHERE kanto = 'true')), 
-        ('charmander', 2, 4, (SELECT id FROM region WHERE kanto = 'true')), 
-        ('charmeleon', 2 ,5, (SELECT id FROM region WHERE kanto = 'true')),
-        ('charizard', 3 ,6, (SELECT id FROM region WHERE kanto = 'true')),
-        ('squirtle', 4 ,7, (SELECT id FROM region WHERE kanto = 'true')),
-        ('wartortle', 4 ,8,(SELECT id FROM region WHERE kanto = 'true')),
-        ('blastoise', 4, 9,(SELECT id FROM region WHERE kanto = 'true')),
-        ('caterpir', 5, 10, (SELECT id FROM region WHERE kanto = 'true')), 
-        ('metapod',  5, 11, (SELECT id FROM region WHERE kanto = 'true')),
-        ('butterfree', 6, 12, (SELECT id FROM region WHERE kanto = 'true')), 
-        ('weedle', 7, 13, (SELECT id FROM region WHERE kanto = 'true')),
-        ('kakuna', 7, 14, (SELECT id FROM region WHERE kanto = 'true')),
-        ('beedrill', 7, 15, (SELECT id FROM region WHERE kanto = 'true')),
-        ('pidgey', 8, 16, (SELECT id FROM region WHERE kanto = 'true')), 
-        ('pidgeotto', 8, 17, (SELECT id FROM region WHERE kanto = 'true')),
-        ('pidgeot', 8, 18, (SELECT id FROM region WHERE kanto = 'true')),
-        ('rattata', 9, 19, (SELECT id FROM region WHERE kanto = 'true')),
-        ('raticate', 9, 20,(SELECT id FROM region WHERE kanto = 'true')); 
+INSERT INTO pokedex (pname, ptype, regionID) 
+    VALUES ('bulbasaur', 1, (SELECT id FROM region WHERE kanto = 'true')),
+        ('ivysaur', 1, (SELECT id FROM region WHERE kanto = 'true')),
+        ('venusaur', 1,  (SELECT id FROM region WHERE kanto = 'true')), 
+        ('charmander', 2, (SELECT id FROM region WHERE kanto = 'true')), 
+        ('charmeleon', 2, (SELECT id FROM region WHERE kanto = 'true')),
+        ('charizard', 3, (SELECT id FROM region WHERE kanto = 'true')),
+        ('squirtle', 4, (SELECT id FROM region WHERE kanto = 'true')),
+        ('wartortle', 4, (SELECT id FROM region WHERE kanto = 'true')),
+        ('blastoise', 4, (SELECT id FROM region WHERE kanto = 'true')),
+        ('caterpir', 5, (SELECT id FROM region WHERE kanto = 'true')), 
+        ('metapod',  5, (SELECT id FROM region WHERE kanto = 'true')),
+        ('butterfree', 6, (SELECT id FROM region WHERE kanto = 'true')), 
+        ('weedle', 7, (SELECT id FROM region WHERE kanto = 'true')),
+        ('kakuna', 7, (SELECT id FROM region WHERE kanto = 'true')),
+        ('beedrill', 7, (SELECT id FROM region WHERE kanto = 'true')),
+        ('pidgey', 8, (SELECT id FROM region WHERE kanto = 'true')), 
+        ('pidgeotto', 8, (SELECT id FROM region WHERE kanto = 'true')),
+        ('pidgeot', 8, (SELECT id FROM region WHERE kanto = 'true')),
+        ('rattata', 9, (SELECT id FROM region WHERE kanto = 'true')),
+        ('raticate', 9, (SELECT id FROM region WHERE kanto = 'true')); 
 
 
 
---
+++ site/app.js
@@ -0,0 +1,78 @@
+// set up:
+var cors = require('cors')
+var express = require('express')
+var app = express()
+PORT = 9351
+var fs = require('fs')
+var db = require('./db-connector')
+
+app.use(express.json())
+app.use(express.static('public'))
+app.use(cors())
+
+//routes:
+app.get('/', function(req, res) {
+    res.status(201).sendFile(__dirname + 'public/pokedex.html')
+})
+
+app.get('/api/pokedex/get', function(req, res) {
+    console.log('get request made')
+    query = 'SELECT * FROM pokedex;'
+    db.pool.query(query, function(err, result, fields) {
+        if (err) {
+            console.log(err)
+            res.status(404)
+        }
+        console.log(result)
+        res.status(201).send(JSON.stringify(result))
+    })
+})
+
+app.post('/api/pokedex/add', function(req, res) {
+    console.log('post request made')
+    query = "INSERT INTO pokedex (pname, ptype, regionID) VALUES ('" + req.body.pname + "', '" + req.body.ptype + "', '" + req.body.regionID + "');"
+    db.pool.query(query, function(err, result, fields) {
+        if (err) {
+            console.log(err)
+            res.status(404)
+        }
+        console.log(result)
+        res.status(201).send(JSON.stringify(result))
+    })
+})
+
+app.delete(`/api/pokedex/delete/:pname`, function(req, res) {
+    console.log('delete request made')
+    query = "DELETE FROM pokedex WHERE pokedexID = " + req.params.pokedexID + ';'
+    db.pool.query(query, function(err, result, fields) {
+        if (err) {
+            console.log(err)
+            res.status(404)
+        }
+        console.log(result)
+        res.status(202).send("pokedex entry deleted")
+    })
+})
+
+app.put(`/api/pokedex/update/:accID`, function(req, res) {
+    console.log('put request made')
+    query = "UPDATE pokedex SET pname = '" + req.body.pname + "', ptype = '" + req.body.ptype + "', regionID = '" + req.body.regionID + "' where pokedexID = " + req.params.pokedexID + ";"
+    //console.log(req)
+    db.pool.query(query, function(err, result, fields) {
+        if (err) {
+            console.log(err)
+            res.status(404)
+        }
+        console.log(result)
+        res.status(202).send("account updated")
+    })
+})
+
+
+/*
+* LISTENER
+*/
+
+app.listen(PORT, function() {
+    console.log('Listening on PORT ' + PORT)
+})
