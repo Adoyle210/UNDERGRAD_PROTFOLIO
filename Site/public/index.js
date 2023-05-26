580b7013151963d05f788d1503cc21eaeec0d777
Adoyle210 <40745653+Adoyle210@users.noreply.github.com>
2023-05-25 21:29:36 -0700

    added index file for the script of pokedex

---
+++ Site/public/index.js
@@ -0,0 +1,50 @@
+const pokedexTable = document.getElementById("pokedexTable");
+
+function displaypokedex() {
+    pokedexTable.innerHTML = ""; 
+    fetch(" ")
+        .then(response => response.json())
+        .then(pokedex => {
+            pokedex.forEach(pokedex => {
+                const row = document.createElement("tr");
+                const pokedexIDCell = document.createElement("td");
+                pokedexIDCell.textContent = pokedex.pokedexID;
+                pokedexIDCell.setAttribute('id', 'data-pokedex-id');
+                row.appendChild(pokedexIDCell);
+
+                const pnameCell = document.createElement("td");
+                pnameCell.textContent = pokedex.pname;
+                row.appendChild(pnameCell);
+
+                const ptypeCell = document.createElement("td");
+                ptypeCell.textContent = pokedex.ptype;
+                row.appendChild(ptypeCell);
+
+                const regionIDCell = document.createElement("td");
+                regionIDCell.textContent = pokedex.regionID;
+                row.appendChild(regionIDCell);
+
+                const actionsCell = document.createElement("td");
+                const deleteButton = document.createElement("button");
+                deleteButton.textContent = "Delete";
+                deleteButton.addEventListener("click", () => {
+                    deletepokedex(pokedex.pokedexID);
+                });
+                actionsCell.appendChild(deleteButton);
+
+                const updateButton = document.createElement("button");
+                updateButton.textContent = "Update";
+                updateButton.addEventListener("click", () => {
+                    updatepokedex(pokedex);
+                });
+                actionsCell.appendChild(updateButton);
+
+                row.appendChild(actionsCell);
+
+                pokedexTable.appendChild(row);
+            });
+        })
+        .catch(error => {
+            console.error("Error fetching pokedex:", error);
+        });
+}
